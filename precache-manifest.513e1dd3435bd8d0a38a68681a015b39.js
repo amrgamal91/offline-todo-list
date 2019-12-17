870be91e7e4dfdba1d1ca186476fc0c0ac35340e
Amr Gamal <ag.amrgamal91@gmail.com>
2019-12-17 18:03:20 +0300

    Updates

---
+++ precache-manifest.513e1dd3435bd8d0a38a68681a015b39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317e407186e9aec595c2cbfb397ae041",
+    "url": "/offline-todo-list/index.html"
+  },
+  {
+    "revision": "f6dc9f095dfab2dc7a0e",
+    "url": "/offline-todo-list/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5cf66e03e137a017cfef",
+    "url": "/offline-todo-list/static/css/main.cdc13419.chunk.css"
+  },
+  {
+    "revision": "f6dc9f095dfab2dc7a0e",
+    "url": "/offline-todo-list/static/js/2.f763b4d0.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/offline-todo-list/static/js/2.f763b4d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cf66e03e137a017cfef",
+    "url": "/offline-todo-list/static/js/main.5cbef4bb.chunk.js"
+  },
+  {
+    "revision": "da5c6c8eac31c3be51fe",
+    "url": "/offline-todo-list/static/js/runtime-main.90964a70.js"
+  },
+  {
+    "revision": "b98c376fd8a25c4a4470a4cea7dc1417",
+    "url": "/offline-todo-list/static/media/background-1.b98c376f.jpg"
+  }
+]);
